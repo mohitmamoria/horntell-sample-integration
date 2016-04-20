5751c36b0843ecf8bbba63fb9658e156cf427b58
Mohit Mamoria <mohit.mamoria@gmail.com>
2016-04-20 09:29:03 +0000

    dumping whole payload

--- webhook.php
+++ webhook.php
@@ -1,17 +1,11 @@
 <?php
 
-// file_put_contents('webhooked.txt', $_POST['horntell_event'] . PHP_EOL, FILE_APPEND);
-
 require 'vendor/autoload.php';
 
 try {
 	$payload = (new Horntell\Event)->fromWebhook();
 
-	if($payload['type'] == 'card.responded') {
-		file_put_contents('webhooked.txt', json_encode($payload['resource']).PHP_EOL);
-	} else {
-		file_put_contents('webhooked.txt', $payload.PHP_EOL);
-	}
+	file_put_contents('webhooked.txt', json_encode($payload).PHP_EOL);
 } catch(Horntell\Errors\Error $e) {
     var_dump($e->getMessage());
 }
