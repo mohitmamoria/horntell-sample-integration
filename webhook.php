d4c8ac5466c0471a45f0abc9dcf1d68dc90868ad
Mohit Mamoria <mohit.mamoria@gmail.com>
2016-04-20 08:19:31 +0000

    dumping whole payload

--- webhook.php
+++ webhook.php
@@ -7,6 +7,8 @@ try {
 
 	if($payload['type'] => 'card.responded') {
 		file_put_contents('webhooked.txt', $payload['resource']['response']['type'] . ' ' . $payload['resource']['response']['value'].PHP_EOL);
+	} else {
+		file_put_contents('webhooked.txt', $payload.PHP_EOL);
 	}
 } catch(Horntell\Errors\Error $e) {
     var_dump($e->getMessage());
